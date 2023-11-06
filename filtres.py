69f194487bea1f2dc5b04c67dceeb51e7329ba04
tosu! <theo.constant42@gmail.com>
2023-11-06 12:28:55 +0100

    Filters for png

--- filtres.py
+++ filtres.py
@@ -1,9 +1,31 @@
 from PIL import Image
 
+kernels = {
+  'blur': [
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+  ],
+  'contrast': [
+    [0, 0, 0, 0, 0],
+    [0, 0, -1, 0, 0],
+    [0, -1, 5, -1, 0],
+    [0, 0, -1, 0, 0],
+    [0, 0, 0, 0, 0],
+  ],
+  'border': [
+    [0, 0, 0],
+    [-1, 1, 0],
+    [0, 0, 0],
+  ]
+}
+
 def _convolve(K, M):
   return sum(sum(K[i][j]*M[i][j] for j in range(len(K))) for i in range(len(K)))
 
-def _idkyet(K, pixels, w, h):
+def _convolve_pixels(K, pixels, w, h):
   n = len(K)
   m = (n-1)//2
   t = sum(sum(abs(K[i][j]) for j in range(len(K))) for i in range(len(K)))
@@ -23,44 +45,22 @@ def _idkyet(K, pixels, w, h):
 
   return new_pixels
 
-
-
 def apply_filter(kernel, img: Image, save_path: str):
   print("Applying filter...")
+  img = img.convert('RGBA')
   w, h = img.size
-  img_r, img_g, img_b = img.split()
-  r, g, b = [_idkyet(kernel, list(elt.getdata()), w, h) for elt in img.split()]
+  img_r, img_g, img_b, img_a = img.split()
+  r = _convolve_pixels(kernel, list(img_r.getdata()), w, h)
+  g = _convolve_pixels(kernel, list(img_g.getdata()), w, h)
+  b = _convolve_pixels(kernel, list(img_b.getdata()), w, h)
   img_r.putdata(r)
   img_g.putdata(g)
   img_b.putdata(b)
-  new_img = Image.merge('RGB', (img_r, img_g, img_b))
+  new_img = Image.merge('RGBA', (img_r, img_g, img_b, img_a))
   new_img.save(save_path)
   print("DONE")
 
 
-
-kernel_blur = [
-    [0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0],
-    [0, 1, 1, 1, 0],
-    [0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0],
-]
-
-kernel_contrast = [
-    [0, 0, 0, 0, 0],
-    [0, 0, -1, 0, 0],
-    [0, -1, 5, -1, 0],
-    [0, 0, -1, 0, 0],
-    [0, 0, 0, 0, 0],
-]
-
-kernel_border = [
-    [0, 0, 0],
-    [-1, 1, 0],
-    [0, 0, 0],
-]
-
-
-img = Image.open('paysage.jpg')
-apply_filter(kernel_blur, img, 'save.jpg')
+if __name__ == '__main__':
+  img = Image.open('ordinateur.png')
+  apply_filter(kernels['blur'], img, 'save.png')
